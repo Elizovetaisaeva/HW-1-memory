317b46bbb50276cd73d9865392de480bbf26ff52
Лаисат <lz_256@mail.ru>
2024-02-15 12:18:58 +0300

    1-memory

--- src/GryffndorStudent.java
+++ src/GryffndorStudent.java
@@ -1,5 +1,23 @@
-public class GryffndorStudent {
-    int nobility;
-    int honor;
-    int bravery;
+public class GryffindorStudent extends HogwartsStudent{
+   private final int nobility;
+    private final  int honor;
+    private final int bravery;
+
+    public GryffindorStudent(String name, int magicPower, int transgressionDistance,
+                             int nobility, int honor, int bravery) {
+        this.nobility = nobility;
+        this.honor = honor;
+        this.bravery = bravery;
+    }
+
+    public int getNobility() {
+        return nobility;
+    }
+    public int getHonor() {
+        return honor;
+    }
+    public int getBravery() {
+        return bravery;
+    }
 }
+
