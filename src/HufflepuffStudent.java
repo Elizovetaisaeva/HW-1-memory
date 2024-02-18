830901fd2e6bff1b57c997e27212f6af95d524c4
Лаисат <lz_256@mail.ru>
2024-02-18 14:28:34 +0300

    1-memory

--- src/HufflepuffStudent.java
+++ src/HufflepuffStudent.java
@@ -1,2 +1,47 @@
-public class HufflepuffStudent {
+public class HufflepuffStudent extends  HogwartsStudent {
+    private final int hardworking;
+    private final int loyal;
+    private final int honest;
+
+    public HufflepuffStudent(String name, int magicPower, int transgressionDistance,
+                             int hardworking, int loyal, int honest) {
+        super(name, magicPower, transgressionDistance);
+        this.hardworking = hardworking;
+        this.loyal = loyal;
+        this.honest = honest;
+    }
+
+    public void innerCompareTo(HufflepuffStudent other) {
+
+        public int getHardworking () {
+            return hardworking;
+        }
+
+        public int getLoyal () {
+            return loyal;
+        }
+
+        public int getHonest () {
+            return honest;
+        }
+        if (this.skillSum() > other.skillSum()) {
+            System.out.println(this.getName() + "сильнее чем" + other.getName());
+        } else if (skillSum() < other.skillSum()) {
+            System.out.println(other.getName() + "сильнее чем" + this.getName());
+        } else {
+            System.out.println(this.getName() + "и" + other.getName() + "равны по силе");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", hardworking=" + hardworking +
+                ", loyal=" + loyal +
+                ", honest=" + honest;
+    }
+
+    private int skillSum() {
+        return hardworking + loyal + honest;
+    }
 }
